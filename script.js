90625812915a07d0c9d5d5b7eb21b45a4d5aab12
Your Namekshai-7 <akshai2537@gmail.com>
2023-06-01 11:30:50 +0530

    first commit

--- script.js
+++ script.js
@@ -124,75 +124,75 @@
 // }
 
 // do whilelooplet i = 1;
- let i = 50;
- do{
-    console.log(i);
-    i++;
-} while (i < 50)  
+//  let i = 50;
+//  do{
+//     console.log(i);
+//     i++;
+// } while (i < 50)  
 
 //for loop 
-for (i = 1; i <= 10; i++){
-        console.log(i);
-}
+// for (i = 1; i <= 10; i++){
+//         console.log(i);
+// }
 
 
 
 // functions
-email="akshai@gmail.com"
-console.log(email.slice(0, email.indexOf('@')));
+// email="akshai@gmail.com"
+// console.log(email.slice(0, email.indexOf('@')));
 
 
-function getName(email) {
-    return(email.slice(0, email.indexOf('@')));
-}
-console.log(getName("akshai2537@gmail.com")); 
-console.log(getName("akshai@gmail.com"));
-console.log(getName("akshaikumar@gmail.com"));
-console.log(getName("akshai25@gmail.com"));
+// function getName(email) {
+//     return(email.slice(0, email.indexOf('@')));
+// }
+// console.log(getName("akshai2537@gmail.com")); 
+// console.log(getName("akshai@gmail.com"));
+// console.log(getName("akshaikumar@gmail.com"));
+// console.log(getName("akshai25@gmail.com"));
 
 
-function toProperCase(name) {
-    return name.charAt(0).toUpperCase()+ name.slice(1).toLowerCase();
-}
-console.log(toProperCase('akshaikumar'));
+// function toProperCase(name) {
+//     return name.charAt(0).toUpperCase()+ name.slice(1).toLowerCase();
+// }
+// console.log(toProperCase('akshaikumar'));
 
 
 //scope  var let const
 //global scope
-var x = 1;
-let y = 2;
-const z = 3;
+// var x = 1;
+// let y = 2;
+// const z = 3;
 
-console.log(`global:${x}`);
-console.log(`global:${y}`);
-console.log(`global:${z}`);
+// console.log(`global:${x}`);
+// console.log(`global:${y}`);
+// console.log(`global:${z}`);
 
 //local scope
-function myFunc() {
-const z = 4;
-    if (true) {
-        var y = 5;
-        console.log(`block:${x}`);
-        console.log(`block:${y}`);
-        console.log(`block:${z}`);
-    }
+// function myFunc() {
+// const z = 4;
+//     if (true) {
+//         var y = 5;
+//         console.log(`block:${x}`);
+//         console.log(`block:${y}`);
+//         console.log(`block:${z}`);
+//     }
   
-    console.log(`function:${x}`);
-console.log(`function:${y}`);
-console.log(`function:${z}`);
-}
-myFunc()
+//     console.log(`function:${x}`);
+// console.log(`function:${y}`);
+// console.log(`function:${z}`);
+// }
+// myFunc()
 
 
 
 //arrays
 
-const myArray = [];
+// const myArray = [];
 
-myArray[0] = 1;
-myArray[1] = 2;
-myArray[2] = 3;
-console.log(myArray);
+// myArray[0] = 1;
+// myArray[1] = 2;
+// myArray[2] = 3;
+// console.log(myArray);
 // myArray.push('4');
 // console.log(myArray);
 // myArray.pop();
@@ -209,11 +209,54 @@ console.log(myArray);
 // console.log(newArray);
 // console.log(myArray.reverse());
 
-myArrayA = ["false", 46, 287]; console.log(myArrayA);
-const Array1 = myArray.concat(myArrayA);
-const Array2 = [...myArray,...myArrayA];
-const Array3 = [myArray,myArrayA];
-console.log(Array1);
-console.log(Array2);
-console.log(Array3);
-console.log(Array3[1][1],Array3[0][1]);
+// myArrayA = ["false", 46, 287]; console.log(myArrayA);
+// const Array1 = myArray.concat(myArrayA);
+// const Array2 = [...myArray,...myArrayA];
+// const Array3 = [myArray,myArrayA];
+// console.log(Array1);
+// console.log(Array2);
+// console.log(Array3);
+// console.log(Array3[1][1], Array3[0][1]);
+
+
+//objects
+//key:value 
+
+// const myobj = { name: "Akshai" };
+// const myobj2 = {
+//     content: {
+//         no: 10000,
+//         name: "Akshai"
+//     },
+//     value: function () {
+//         // return "Hello World";
+//         return `Hello ${this.content.name}`;
+//     }
+// };
+// console.log(myobj.name);
+// console.log(myobj2.value());
+
+const vehicle = {
+    door: 2,
+    engin: function () {
+        return "Vrooomm";
+    }
+}
+console.log(vehicle.engin());
+
+const car = Object.create(vehicle);
+console.log(car.engin(), car.door);
+
+const movie = {
+    actor: "kavin",
+    music: "Arr",
+    director: "Lokesh"
+}
+console.log(Object.values(movie));
+console.log(movie.hasOwnProperty('actor'));
+
+// destructerobeject
+function signs({music}) {
+    return music
+}
+console.log(signs(movie));
